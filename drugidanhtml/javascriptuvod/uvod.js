affb18828072956255604a173eed4d48e12a4c5b
mtemp111 <144005864+mtemp111@users.noreply.github.com>
2023-09-05 22:25:21 +0200

    Add files via upload

---
+++ drugidanhtml/javascriptuvod/uvod.js
@@ -0,0 +1,50 @@
+// // alert("yoyoyo");
+// console.log("HELLO WORLD ");
+// let x = 5;
+// const imeprezime = "Marko Markovic";
+
+// let x;
+// console.log(x);
+// console.log(typeof(x));
+
+// let niz = [1,2,3,4,5];
+// let objekat = {
+//     ime : "Ivan",
+//     prezime : "Prezimenovic",
+//     prosjek : 8.5,
+
+
+// }
+// console.log(ojekat.ime)
+// console.log(objekat["ime"])
+
+
+// for (let i =0; i < array.lenghti++){
+
+// }
+
+// function saberi(a,b){
+//     return a+b
+// }
+// console.log(saberi(4,5));
+
+let x = [{
+    ime : "Ivan",
+    prezime : "Prezimenovic",
+    prosjek : 9.5},{
+        ime : "Ivan",
+        prezime : "Prezimenovic",
+        prosjek : 6.5},{
+            ime : "Ivan",
+            prezime : "Prezimenovic",
+            prosjek : 5.5}
+]
+function provjera(lista){
+for (let i in x){
+    if( i[prosjek]>8.5){
+        return
+    }}
+
+}
+
+console.log(provjera(x))
